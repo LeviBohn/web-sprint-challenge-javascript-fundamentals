a440cf9e3df1abdd7c6849df743ecec717eef65f
Levi Bohn <levibohn@gmail.com>
2022-11-08 05:41:34 -0600

    succussfully completed Request 1 for animalNames.

--- index.js
+++ index.js
@@ -67,10 +67,15 @@ const zooAnimals = [
   💡 NOTE: the array returned should be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function animalNames(zooAnimals) {
+    const displayNames = [];
+    zooAnimals.forEach ((zooAnimals) => {
+      displayNames.push(`name: ${zooAnimals.animal_name}, scientific: ${zooAnimals.scientific_name}`);
+    })
+    return (displayNames);
   }
-  
+
+console.log('Request 1:', animalNames(zooAnimals));
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
   The zoo needs a list of all their animal's names converted to lower case. 
